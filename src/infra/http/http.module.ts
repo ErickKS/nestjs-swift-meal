b9d442960d4a0c1a29dc8b2b8f4008f0d7fc0672
Erick <erick.kuwahara79@gmail.com>
2025-06-05 18:41:56 -0300

    feat: add reactivate-category controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common'
 import { CreateCategoryUseCase } from '@/application/category/use-cases/create-category'
 import { DeleteCategoryUseCase } from '@/application/category/use-cases/delete-category'
 import { FetchCategoriesUseCase } from '@/application/category/use-cases/fetch-categories'
+import { ReactivateCategoryUseCase } from '@/application/category/use-cases/reactivate-category'
 import { UpdateCategoryUseCase } from '@/application/category/use-cases/update-category'
 import { DatabaseModule } from '../database/database.module'
 import { HealthCheckController } from './crontrollers/app/health-check.controller'
 import { CreateCategoryController } from './crontrollers/category/create-category.controller'
 import { DeleteCategoryController } from './crontrollers/category/delete-category.controller'
 import { FetchCategoriesController } from './crontrollers/category/fetch-categories.controller'
+import { ReactivateCategoryController } from './crontrollers/category/reactivate-category.controller'
 import { UpdateCategoryController } from './crontrollers/category/update-category.controller'
 
 @Module({
@@ -19,7 +21,8 @@ import { UpdateCategoryController } from './crontrollers/category/update-categor
     FetchCategoriesController,
     UpdateCategoryController,
     DeleteCategoryController,
+    ReactivateCategoryController,
   ],
-  providers: [CreateCategoryUseCase, FetchCategoriesUseCase, UpdateCategoryUseCase, DeleteCategoryUseCase],
+  providers: [CreateCategoryUseCase, FetchCategoriesUseCase, UpdateCategoryUseCase, DeleteCategoryUseCase, ReactivateCategoryUseCase],
 })
 export class HttpModule {}
